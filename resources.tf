ff66fc7980a0d78898bbbe12e3f0d11d2e9fb813
or <orow1993@gmail.com>
2024-06-20 14:21:52 +0300

    vpc and subnet created

--- resources.tf
+++ resources.tf
@@ -1,3 +1,11 @@
 resource "google_compute_network" "vpc_restore" {
   name = "vpc_restore"
 }
+
+resource "google_compute_subnetwork" "subnet_restore" {
+  name          = "subnet_restore"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "europe-west3"
+  network       = google_compute_network.vpc_restore
+  
+}
