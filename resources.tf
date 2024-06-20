dbd6d58fb7f8349c442542b2309bd2ad09e131f6
or <orow1993@gmail.com>
2024-06-20 15:06:24 +0300

    all resources created

--- resources.tf
+++ resources.tf
@@ -1,11 +1,33 @@
-resource "google_compute_network" "vpc_restore" {
-  name = "vpc_restore"
+resource "google_compute_network" "vpc-restore" {
+  name = "vpc-restore"
+   auto_create_subnetworks = "false"
 }
 
-resource "google_compute_subnetwork" "subnet_restore" {
-  name          = "subnet_restore"
+resource "google_compute_subnetwork" "subnet-restore" {
+  name          = "subnet-restore"
   ip_cidr_range = "10.0.1.0/24"
   region        = "europe-west3"
-  network       = google_compute_network.vpc_restore
+  network       = google_compute_network.vpc-restore.name
   
+}
+
+
+
+resource "google_compute_instance" "vm-restore" {
+
+  name         = "vm-restore"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "projects/windows-cloud/global/images/windows-server-2022-dc-v20240516"
+    }
+
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet-restore.id
+    access_config {}
+}
 }
