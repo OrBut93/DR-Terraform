6de60de12c0a4e262ef45bbb9b169ca0d2c3fdb9
or <orow1993@gmail.com>
2024-07-01 17:56:18 +0300

    second provider

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ provider "google" {
   region  = "var.region"
 }
 
+# Provider for the network project
+provider "google" {
+  alias       = "network_project"
+  credentials = file("<path-to-existing-project-service-account-key>.json")
+  project     = "network-project-id"
+  region      = "us-central1"
+}
+
