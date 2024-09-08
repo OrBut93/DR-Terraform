88b88316a9cdb32250311494418af796263cd10a
or <orow1993@gmail.com>
2024-09-08 12:02:17 +0300

    add credentials path

--- main.tf
+++ main.tf
@@ -1,23 +1,76 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.33.0"
-    }
-  }
-}
-
+# configure backup project  
 provider "google" {
-  # Configuration options
-  project = "orprojectestt"
-  region  = "var.region"
+  credentials = file("<path_to_service_account_key_file>.json")
+  project     = "<backup_project_id>"
+  region      = var.region
 }
 
-# Provider for the network project
+# configure network project  
 provider "google" {
-  alias       = "network_project"
-  credentials = file("<path-to-existing-project-service-account-key>.json")
-  project     = "network-project-id"
-  region      = "us-central1"
+  alias       = "network"
+  credentials = file("<path_to_service_account_key_file>.json")
+  project     = "<network_project_id>"
+  region      = var.region
+}
+# configure production project  
+provider "google" {
+  alias       = "production"
+  credentials = file("<path_to_service_account_key_file>.json")
+  project     = "<production_project_id>"
+  region      = var.region
 }
 
+# Create VPC peering between backup and network
+resource "google_compute_network_peering" "backup_to_network" {
+  provider        = google
+  name            = "backup-to-network"
+  network         = "backup-vpc"
+  peer_network    = "projects/<network_project_id>/global/networks/network-vpc"
+  export_custom_routes = true
+  import_custom_routes = true
+}
+
+resource "google_compute_network_peering" "network_to_backup" {
+  provider        = google.network
+  name            = "network-to-backup"
+  network         = "network-vpc"
+  peer_network    = "projects/<backup_project_id>/global/networks/backup-vpc"
+  export_custom_routes = true
+  import_custom_routes = true
+}
+
+# Create VPC peering between network and production
+resource "google_compute_network_peering" "network_to_production" {
+  provider        = google.network
+  name            = "network-to-production"
+  network         = "network-vpc"
+  peer_network    = "projects/<production_project_id>/global/networks/production-vpc"
+  export_custom_routes = true
+  import_custom_routes = true
+}
+
+resource "google_compute_network_peering" "production_to_network" {
+  provider        = google.production
+  name            = "production-to-network"
+  network         = "production-vpc"
+  peer_network    = "projects/<network_project_id>/global/networks/network-vpc"
+  export_custom_routes = true
+  import_custom_routes = true
+}
+
+# Output Peering Information
+output "backup_to_network_status" {
+  value = google_compute_network_peering.backup_to_network.state
+}
+
+output "network_to_backup_status" {
+  value = google_compute_network_peering.network_to_backup.state
+}
+
+output "network_to_production_status" {
+  value = google_compute_network_peering.network_to_production.state
+}
+
+output "production_to_network_status" {
+  value = google_compute_network_peering.production_to_network.state
+}
--- variables.tf
+++ variables.tf
@@ -8,3 +8,28 @@ variable "region" {
     default = "europe-west3"
   
 }
+
+####################
+#CREDENTIALS
+####################
+variable "backup_service_account_key_path" {
+  description = "Backup project - The path to the service account key file"
+  type        = string
+  default     = "/path/to/your/service-account-key.json"  # Set default or provide value via CLI
+}
+
+
+variable "network_service_account_key_path" {
+  description = "Network project - The path to the service account key file"
+  type        = string
+  default     = "/path/to/your/service-account-key.json"  # Set default or provide value via CLI
+}
+
+
+variable "production_service_account_key_path" {
+  description = "Production project - The path to the service account key file"
+  type        = string
+  default     = "/path/to/your/service-account-key.json"  # Set default or provide value via CLI
+}
+
+####################
